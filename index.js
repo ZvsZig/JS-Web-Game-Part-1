4ced69247e22b84f2f83ae95624d9275a5fe787b
zig <zvszig@gmail.com>
2023-09-21 18:06:05 -0700

    finished product without background

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+const character = (image, leftPos, botPos) => {
+let gameAsset = document.createElement('img')
+gameAsset.src = image
+gameAsset.style.position = 'fixed'
+gameAsset.style.left = leftPos
+gameAsset.style.bottom = botPos
+document.body.append(gameAsset)
+}
+
+
+character('assets/green-character.gif', '100px', '100px')
+character('assets/pine-tree.png', '450px', '200px')
+character('assets/pillar.png', '350px', '100px')
+character('assets/crate.png', '150px', '200px')
+character('assets/well.png', '500px', '425px')
+
+const newItem = (pic, x, y) => {
+let equipment = document.createElement('img')
+equipment.src = pic
+equipment.style.position = 'fixed'
+equipment.style.left = x
+equipment.style.bottom = y
+document.body.append(equipment)
+    
+equipment.addEventListener('dbclick', function(){
+    equipment.remove()
+}) 
+}
+
+newItem('assets/sword.png', '500px', '405px')
+newItem('assets/shield.png', '165px', '185px')
+newItem('assets/staff.png', '600px', '100px')
